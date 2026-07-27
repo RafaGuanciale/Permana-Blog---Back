1ed811c989a1adbc647a0c352bf2785c6361e2a6
Rafael <rafaelguanciale@hotmail.com>
2026-07-27 14:26:53 -0300

    Rrefactor: Cors ajustado

--- src/app.ts
+++ src/app.ts
@@ -5,17 +5,19 @@ import cors from "cors";
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: ['http://localhost:3001', 'https://permana-blog-front.vercel.app/']
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:3001", "https://permana-blog-front.vercel.app/"],
+  }),
+);
 
 app.get("/", (req, res) => {
   res.json({ message: "Permana Blog API" });
 });
 
-app.use('/posts', router);
+app.use("/posts", router);
 app.use((req, res) => {
-  res.status(404).send('Not found');
+  res.status(404).send("Not found");
 });
 
 export default app;
