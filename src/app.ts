c604d82ade2175fcd7ad40853dfb55f6fa5c0d86
Rafael <rafaelguanciale@hotmail.com>
2026-07-27 13:10:59 -0300

    Feat: Cors corrigido para o render

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,9 @@ import cors from "cors";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3001', 'https://permana-blog-back.onrender.com']
+}));
 
 app.get("/", (req, res) => {
   res.json({ message: "Permana Blog API" });
